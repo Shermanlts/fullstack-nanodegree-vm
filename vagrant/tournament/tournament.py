558f528d50cfaafa17127298065d8e47979d057c
Rob Sherman <Shermanlts@gmail.com>
2016-08-22 18:02:22 -0700

    Cleaned up notes and added a missing close to the last function

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -16,6 +16,7 @@ def deleteMatches():
     DB= connect()
     c = DB.cursor()
     c.execute("DELETE from matches")
+    # Removes all standings when matches are deleted
     c.execute("UPDATE standings SET wins=0, matches = 0")
     DB.commit()
     DB.close()
@@ -54,8 +55,10 @@ def registerPlayer(name):
     """
     DB= connect()
     c = DB.cursor()
+    # Inserts players name and checks against SQL attacks
     c.execute("INSERT into players(name) values (%s)", (bleach.clean(name),))
     DB.commit()
+    # Gets the latest id(generated) and add that player to the standings table
     c.execute("SELECT id from players order by id desc")
     latest = c.fetchone()[0]
     c.fetchall()
@@ -79,7 +82,7 @@ def playerStandings():
     """
     DB = connect()
     c = DB.cursor()
-    c.execute("SELECT players.id, players.name, standings.wins, standings.matches from players join standings on players.id = standings.id order by wins desc, omw desc")
+    c.execute("SELECT players.id, players.name, standings.wins, standings.matches from players join standings on players.id = standings.id order by wins desc")
     rows = c.fetchall()
     return rows
     DB.close()
@@ -94,6 +97,7 @@ def reportMatch(winner, loser):
     """
     DB = connect()
     c = DB.cursor()
+    # Adds data to matches and then updates the standings table with that data
     c.execute("INSERT into matches(winner,loser) values (%s,%s)" %(winner, loser))
     c.execute("UPDATE standings SET wins = (wins+1) where id =%s" % winner)
     c.execute("UPDATE Standings SET matches = (matches+1) where (id = %s) or (id= %s)" % (winner, loser))
@@ -118,14 +122,15 @@ def swissPairings():
     """
     DB = connect()
     c = DB.cursor()
-    c.execute("SELECT standings.id, players.name from players join standings on players.id=standings.id order by wins desc, omw desc")
+    c.execute("SELECT standings.id, players.name from players join standings on players.id=standings.id order by wins desc")
     pairings = []
     i = 0
     rows = c.fetchall()
+    # loop builds the list of tuples that are then returned
     while i<=(len(rows)-1):
         pairings.append((rows[i][0],rows[i][1],rows[i+1][0],rows[i+1][1]))
         i = i+2 
-    print pairings
+    DB.close()
     return pairings
     
 
