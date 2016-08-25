b00c3a7d07ed2f7f09fe77bf83151dd1812cbc94
Rob Sherman <Shermanlts@gmail.com>
2016-08-25 15:09:58 -0700

    Removed standing table due to redundancy.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,3 +1,4 @@
+
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
@@ -20,14 +21,6 @@ CREATE DATABASE tournament;
 CREATE TABLE players (
 	id serial primary key,
 	name text
-	);
-
---Create Standings table
---Opponent match wins is omw
-CREATE TABLE standings (
-	id integer references players,
-	wins integer default 0,
-	matches integer default 0
 	);
 
 --Create matches table
